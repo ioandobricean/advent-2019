4e5023c6a3c4726eb96ff94df996bfc6d8148f69
Ioan Dobricean <ioan.dobricean@itiviti.com>
2019-12-04 17:16:54 +0200

    Day2 - Part Two
    Add Memory object for the Intcode program
    
    Change-Id: I94135e41b68a6cc705d5cd05f4ba39caadf8505f

--- src/main/kotlin/intcode.kt
+++ src/main/kotlin/intcode.kt
@@ -1,71 +1,103 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException
 
-data class Intcode(val input: String) {
-    fun execute(): Intcode {
-        val split: ArrayList<Int> = ArrayList(input.split(",").map { Integer.valueOf(it) })
-        var opIndex = 0
+data class Intcode(val memory: Memory) {
+    fun execute() {
+        var pointer = 0
         do {
-            val operation = operationFactory(opIndex, split)
-            if (!operation.hasNextOperation()) {
-                break
-            }
-            println(operation)
+            val operation = buildInstruction(pointer, memory)
             operation.execute()
-            opIndex = operation.nextOperationIndex()
-        } while (opIndex < split.size)
-        return Intcode(split.joinToString(separator = ","))
+            pointer = operation.nextInstructionPointer()
+        } while (operation.hasNextOperation())
     }
 
 }
 
-fun operationFactory(opIndex: Int, code: ArrayList<Int>): Operation {
-    val opCode = code[opIndex]
+fun buildInstruction(pointer: Int, memory: Memory): Instruction {
+    val opCode = memory.getAddressValue(pointer)
     return when (opCode) {
-        1 -> AddOperation(opIndex, code)
-        2 -> MultiplyOperation(opIndex, code)
-        99 -> StopOperation()
+        1 -> AddInstruction(pointer, memory)
+        2 -> MultiplyInstruction(pointer, memory)
+        99 -> StopInstruction(pointer)
         else -> throw NotImplementedException()
     }
 }
 
-class StopOperation : Operation() {
+class StopInstruction(val pointer: Int) : Instruction() {
     override fun hasNextOperation(): Boolean = false
-    override fun nextOperationIndex(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun nextInstructionPointer() = pointer + 1
 
     override fun execute() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 }
 
-abstract class Operation {
+abstract class Instruction {
     abstract fun hasNextOperation(): Boolean
-    abstract fun nextOperationIndex(): Int
+    abstract fun nextInstructionPointer(): Int
     abstract fun execute()
 }
 
-data class AddOperation(val opIndex: Int, val code: ArrayList<Int>) : Operation() {
-    override fun hasNextOperation(): Boolean = nextOperationIndex() < code.size
-    override fun nextOperationIndex(): Int = opIndex + 4
+data class AddInstruction(val pointer: Int, val memory: Memory) : Instruction() {
+    override fun hasNextOperation(): Boolean = nextInstructionPointer() < memory.size()
+    override fun nextInstructionPointer(): Int = pointer + 4
 
     override fun execute() {
-        val posInput1 = code[opIndex + 1]
-        val posInput2 = code[opIndex + 2]
-        val posOutput = code[opIndex + 3]
-        code[posOutput] = code[posInput1] + code[posInput2]
-        println("[$posOutput] = [$posInput1] + [$posInput2] = ${code[posInput1]} + ${code[posInput2]}")
+        if (!hasNextOperation()) {
+            println("Buffer Overflow")
+            return
+        }
+        val posInput1 = memory.getAddressValue(pointer + 1)
+        val posInput2 = memory.getAddressValue(pointer + 2)
+        val posOutput = memory.getAddressValue(pointer + 3)
+        val value1 = memory.getAddressValue(posInput1)
+        val value2 = memory.getAddressValue(posInput2)
+        memory.setAddressValue(posOutput, value1 + value2)
     }
 }
 
-data class MultiplyOperation(val opIndex: Int, val code: ArrayList<Int>) : Operation() {
-    override fun hasNextOperation(): Boolean = nextOperationIndex() < code.size
-    override fun nextOperationIndex(): Int = opIndex + 4
+data class MultiplyInstruction(val pointer: Int, val memory: Memory) : Instruction() {
+    override fun hasNextOperation(): Boolean = nextInstructionPointer() < memory.size()
+    override fun nextInstructionPointer(): Int = pointer + 4
     override fun execute() {
-        val posInput1 = code[opIndex + 1]
-        val posInput2 = code[opIndex + 2]
-        val posOutput = code[opIndex + 3]
-        code[posOutput] = code[posInput1] * code[posInput2]
+        if (!hasNextOperation()) {
+            println("Buffer Overflow")
+            return
+        }
+        val posInput1 = memory.getAddressValue(pointer + 1)
+        val posInput2 = memory.getAddressValue(pointer + 2)
+        val posOutput = memory.getAddressValue(pointer + 3)
+        val value1 = memory.getAddressValue(posInput1)
+        val value2 = memory.getAddressValue(posInput2)
+        memory.setAddressValue(posOutput, value1 * value2)
     }
+}
+
+class Memory {
+    private var memory: IntArray
+
+    constructor(input: String) {
+        memory = input.split(",").map { Integer.valueOf(it) }.toIntArray()
+    }
+
+    fun getAddressValue(address: Int) = memory[address]
+    fun setAddressValue(address: Int, value: Int) {
+        memory[address] = value
+    }
+
+    fun size() = memory.size
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Memory) return false
+
+        if (!memory.contentEquals(other.memory)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return memory.contentHashCode()
+    }
+
+    override fun toString() = memory.joinToString(separator = ",")
 }
--- src/test/kotlin/IntcodeSpec.kt
+++ src/test/kotlin/IntcodeSpec.kt
@@ -5,35 +5,50 @@ import org.spekframework.spek2.style.specification.describe
 object IntcodeSpec : Spek({
     describe("An Intcode program") {
         it("adds 2 numbers") {
-            val intcode = Intcode("1,5,6,3,99,10,20")
-            intcode.execute() shouldEqual Intcode("1,5,6,30,99,10,20")
+            val memory = Memory("1,5,6,3,99,10,20")
+            val intcode = Intcode(memory)
+
+            intcode.execute()
+            memory shouldEqual Memory("1,5,6,30,99,10,20")
         }
         it("multiply 2 numbers") {
-            val intcode = Intcode("2,5,6,3,99,10,20")
-            intcode.execute() shouldEqual Intcode("2,5,6,200,99,10,20")
+            val memory = Memory("2,5,6,3,99,10,20")
+            val intcode = Intcode(memory)
+            intcode.execute()
+            memory shouldEqual Memory("2,5,6,200,99,10,20")
         }
         it("add then multiply 2 numbers") {
-            val intcode = Intcode("1,9,10,3,2,9,10,7,99,10,20")
-            intcode.execute() shouldEqual Intcode("1,9,10,30,2,9,10,200,99,10,20")
+            val memory = Memory("1,9,10,3,2,9,10,7,99,10,20")
+            val intcode = Intcode(memory)
+            intcode.execute()
+            memory shouldEqual Memory("1,9,10,30,2,9,10,200,99,10,20")
         }
     }
 
     describe("Intcode test for") {
         it("adds 2 numbers") {
-            val intcode = Intcode("1,0,0,0,99")
-            intcode.execute() shouldEqual Intcode("2,0,0,0,99")
+            val memory = Memory("1,0,0,0,99")
+            val intcode = Intcode(memory)
+            intcode.execute()
+            memory shouldEqual Memory("2,0,0,0,99")
         }
         it("multiply 2 numbers") {
-            val intcode = Intcode("2,3,0,3,99")
-            intcode.execute() shouldEqual Intcode("2,3,0,6,99")
+            val memory = Memory("2,3,0,3,99")
+            val intcode = Intcode(memory)
+            intcode.execute()
+            memory shouldEqual Memory("2,3,0,6,99")
         }
         it("multiply 2 other numbers") {
-            val intcode = Intcode("2,4,4,5,99,0")
-            intcode.execute() shouldEqual Intcode("2,4,4,5,99,9801")
+            val memory = Memory("2,4,4,5,99,0")
+            val intcode = Intcode(memory)
+            intcode.execute()
+            memory shouldEqual Memory("2,4,4,5,99,9801")
         }
         it("add then multiply 2 numbers") {
-            val intcode = Intcode("1,1,1,4,99,5,6,0,99")
-            intcode.execute() shouldEqual Intcode("30,1,1,4,2,5,6,0,99")
+            val memory = Memory("1,1,1,4,99,5,6,0,99")
+            val intcode = Intcode(memory)
+            intcode.execute()
+            memory shouldEqual Memory("30,1,1,4,2,5,6,0,99")
         }
     }
 })
