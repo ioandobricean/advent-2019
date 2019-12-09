cd5a2ae4b7c1f396f5523dd83a56b714ab814746
Ioan Dobricean <ioan.dobricean@gmail.com>
2019-12-09 20:52:33 +0200

    WIP Day7 - Part Two
    - Add extra tests
    - add coroutines dependency

--- src/main/kotlin/amplifier/AmplifierControllerSoftware.kt
+++ src/main/kotlin/amplifier/AmplifierControllerSoftware.kt
@@ -5,14 +5,15 @@ import intcode.Memory
 class AmplifierControllerSoftware {
     fun maxSignal(phaseSettings: List<Int>, memory: Memory): Int {
         val ampA = Amplifier(phaseSettings[0], memory.clone())
-        val outputAmpA = ampA.amplify(0)
         val ampB = Amplifier(phaseSettings[1], memory.clone())
-        val outputAmpB = ampB.amplify(outputAmpA)
         val ampC = Amplifier(phaseSettings[2], memory.clone())
-        val outputAmpC = ampC.amplify(outputAmpB)
         val ampD = Amplifier(phaseSettings[3], memory.clone())
-        val outputAmpD = ampD.amplify(outputAmpC)
         val ampE = Amplifier(phaseSettings[4], memory.clone())
+
+        val outputAmpA = ampA.amplify(0)
+        val outputAmpB = ampB.amplify(outputAmpA)
+        val outputAmpC = ampC.amplify(outputAmpB)
+        val outputAmpD = ampD.amplify(outputAmpC)
         return ampE.amplify(outputAmpD)
     }
 }
